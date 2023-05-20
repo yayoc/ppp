2fabe3107bdbb5c1a469f116f7f56846d8c8d6bf
yayoc <hi@yayoc.com>
2023-05-20 11:19:49 +0900

    add 3.3 TRY

---
+++ chapter3/read_name_and_age.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Please enter your first name and age\n";			
+	string first_name = "???";
+	double age = -1;			
+	cin >> first_name >> age;	
+	cout << "Hello, " << first_name << " (age" << age << ")\n";
+}
+		
