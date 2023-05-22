773fca28f1860f0cb2c7e5aecae87da4c0b9d68b
yayoc <hi@yayoc.com>
2023-05-22 21:18:11 +0900

    replace if-else statements with switch

--- chapter4/try_4.4.cpp
+++ chapter4/try_4.4.cpp
@@ -4,23 +4,24 @@ int main() {
   constexpr double yen_to_doller = 0.0073;
   constexpr double euro_to_doller = 1.08;
   constexpr double pound_to_doller = 1.24;
-
   double amount = 0;
-  string unit = "";
+  char unit = ' ';
 
-  cout << "Please enter an amount followed by a unit(yen, euros, and pounds)\n";
+  cout << "Please enter an amount followed by a unit(y, e, and p)\n";
   cin >> amount >> unit;
 
-  if (unit == "yen") {
-	cout << amount << "yen == " << amount * yen_to_doller << "dollers\n";
-  }
-  else if (unit == "euros") {
-	cout << amount << "euros == " << amount * euro_to_doller << "dollers\n";
-  } 
-  else if (unit == "pounds") {
-	cout << amount << "pounds == " << amount * pound_to_doller << "dollers\n";
-  }
-  else {
-	cout << "Sorry, I don't know a unit called " << unit << "\n";
+  switch (unit) {
+  case 'y':
+    cout << amount << "yen == " << amount * yen_to_doller << "dollers\n";
+    break;
+  case 'e':
+    cout << amount << "euros == " << amount * euro_to_doller << "dollers\n";
+    break;
+  case 'p':
+    cout << amount << "pounds == " << amount * pound_to_doller << "dollers\n";
+    break;
+  default:
+    cout << "Sorry, I don't know a unit called " << unit << "\n";
+    break;
   }
 }
