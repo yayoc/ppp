61e3228d5f2e5625d173f5bdb5376695b71df064
yayoc <hi@yayoc.com>
2023-05-23 15:59:15 +0900

    drill 2

--- chapter4/drill.cpp
+++ chapter4/drill.cpp
@@ -4,13 +4,20 @@ int main() {
   vector<int> nums;
   int val = 0;
   while(cin >> val) {
-    if (val == 124) { // | = 
+    if (val == 124) { // | = 124
+      break;
+    }
+    if (nums.size() >= 2) {
       break;
     }
     nums.push_back(val);
   }
 
-  for (int num : nums) {
-    cout << num << "\n";
+  if (nums[0] > nums[1]) {
+    cout << "the smaller value is: " << nums[1] << "\n";
+    cout << "the bigger value is: " << nums[0] << "\n";
+  } else if (nums[1] > nums[0]){
+    cout << "the smaller value is: " << nums[0] << "\n";
+    cout << "the bigger value is: " << nums[1] << "\n";
   }
 }
