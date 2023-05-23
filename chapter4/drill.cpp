91ec377f4ec488bbea055621fe90cc1fab060e57
yayoc <hi@yayoc.com>
2023-05-24 05:07:43 +0900

    drill 7

--- chapter4/drill.cpp
+++ chapter4/drill.cpp
@@ -1,34 +1,87 @@
 #include "std_lib_facilities.h"
 
+double to_cm(double n, string unit) {
+
+  if (unit == "cm") {
+    return n;
+  } else if (unit == "m") {
+    return n * 100;
+  } else if (unit == "in") {
+    return n * 2.54;
+  } else if (unit == "ft") {
+    return n * 12 * 2.54; // ft = 12in
+  }
+
+  return n;
+}
+
+double str_to_cm(string str) {
+  double numericPart = 0;
+  string unit = "";
+
+  size_t index = str.find_first_not_of("0123456789.");
+  if (index != std::string::npos) {
+    numericPart = stod(str.substr(0, index));
+    unit = str.substr(index);
+  } else {
+    // If there are no non-digit characters, consider the entire string as the
+    // numeric part
+    numericPart = stod(str);
+  }
+
+  return to_cm(numericPart, unit);
+}
+
+bool has_digits_only(string str) {
+  for (char c : str) {
+    if (c == 46) { //
+      continue;
+    }
+    if (c < 48 || c > 57) {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool is_unit(string str) {
+  if (str == "cm" | str == "m" | str == "in" | str == "ft") {
+    return true;
+  }
+  return false;
+}
+
 int main() {
-  vector<double> nums;
-  double val = 0;
-  while(cin >> val) {
-    if (val == 124) { // | = 124
+  double smallest = 1000000;
+  double largest = 0;
+
+  vector<string> nums;
+  string tmp = "";
+
+  for (string str; cin >> str;) {
+    if (str == "|") {
       break;
     }
-    if (nums.size() >= 2) {
-      break;
+    if (has_digits_only(str)) {
+      tmp = str;
+    } else if (is_unit(str)) {
+      nums.push_back(tmp + str);
+      tmp = "";
+    } else {
+      nums.push_back(str);
     }
-    nums.push_back(val);
   }
 
-  double diff = nums[0] - nums[1];
+  for (string num : nums) {
+    double cm = str_to_cm(num);
+    if (cm < smallest) {
+      cout << num << " the smallest so far\n";
+      smallest = cm;
+    }
 
-  if (diff > 0) {
-    cout << "the smaller value is: " << nums[1] << "\n";
-    cout << "the bigger value is: " << nums[0] << "\n";
-  } else if (diff < 0){
-    cout << "the smaller value is: " << nums[0] << "\n";
-    cout << "the bigger value is: " << nums[1] << "\n";
-  } else if (diff == 0) {
-    cout << "the numbers are equal\n";
-  }
-
-  if (diff < 0) {
-    diff = -diff;
-  }
-  if (diff != 0 && diff < 1.0/100) {
-    cout << "the numbers are almost equal";
+    if (cm > largest) {
+      cout << num << " the largest so far\n";
+      largest = cm;
+    }
   }
 }
