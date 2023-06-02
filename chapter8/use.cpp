5c6a801e425f6074111db61d74525bd5f894aa5f
yayoc <hi@yayoc.com>
2023-06-03 08:09:28 +0900

    drill 3

--- chapter8/use.cpp
+++ chapter8/use.cpp
@@ -4,34 +4,40 @@
 
 int foo = 7;
 
+namespace X {
+    int var;
+    void print() {
+        cout << var << "\n";
+    }
+}
+
+namespace Y {
+    int var;
+    void print() {
+        cout << var << "\n";
+    }
+}
+
+namespace Z {
+    int var;
+    void print() {
+        cout << var << "\n";
+    }
+}
+
+
 int main() {
-    print_foo();
-    print(99);
-
-    int x = 7;
-    int y = 9;
-
-    swap_r(x, y);
-    cout << "x is " << x << "\n";
-    cout << "y is " << y << "\n";
-
-    // swap_v does not swap values
-    swap_v(7, 9);
-
-    /*
-    const int cx = 7;
-    const int cy = 9;
-    swap_cr(cx, cy);
-    */
-
-
-    /*
-       can't compile as swap functions take int
-    swap_?(7.7, 9.9);
-    double dx = 7.7;
-    double dy = 9.9;
-    swap_?(dx, dy);
-    swap_?(7.7, 9.9);
-    */
-
+    X::var = 7;
+    X::print();
+    using namespace Y;
+    var = 9;
+    print();
+    {
+        using Z::var;
+        using Z::print;
+        var = 11;
+        print();
+    }
+    print();
+    X::print();
 }
