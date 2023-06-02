cb9f7c9c3d290a08f69983c7537673e9b1f7170d
yayoc <hi@yayoc.com>
2023-06-03 08:04:15 +0900

    drill 2

--- chapter8/my.cpp
+++ chapter8/my.cpp
@@ -10,5 +10,27 @@ void print(int i) {
     cout << i << "\n";
 }
 
+void swap_v(int a, int b) {
+    // this does not swap a and b on caller
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
 
+void swap_r(int& a, int& b) {
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
 
+void swap_cr(const int& a, const int& b) {
+    /*
+       can't compile as const values are read-only
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    */
+}
--- chapter8/use.cpp
+++ chapter8/use.cpp
@@ -1,7 +1,37 @@
 #include "my.h"
+
+#include "../std_lib_facilities.h"
+
 int foo = 7;
 
 int main() {
     print_foo();
     print(99);
+
+    int x = 7;
+    int y = 9;
+
+    swap_r(x, y);
+    cout << "x is " << x << "\n";
+    cout << "y is " << y << "\n";
+
+    // swap_v does not swap values
+    swap_v(7, 9);
+
+    /*
+    const int cx = 7;
+    const int cy = 9;
+    swap_cr(cx, cy);
+    */
+
+
+    /*
+       can't compile as swap functions take int
+    swap_?(7.7, 9.9);
+    double dx = 7.7;
+    double dy = 9.9;
+    swap_?(dx, dy);
+    swap_?(7.7, 9.9);
+    */
+
 }
